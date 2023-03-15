ad1da6ba6fcb4937773a7f6c3169bd8bf2dc8f08
phobal <phobal@126.com>
2023-03-15 20:15:12 +0800

    feat: the map support zoom in and zoom out

---
+++ src/views/MapPage/Controls.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+type Props = {
+  /** deck.gl 官方未给出具体定义， 也是使用的 any */
+  viewState: any
+  setViewState: (viewState: any) => void
+}
+
+/** 控制器按钮 */
+const Button: React.FC<{ symbol: string; onClick: () => void }> = ({ symbol, onClick }) => {
+  return (
+    <div
+      className="flex justify-center items-center h-30px w-30px text-[18px] select-none"
+      onClick={onClick}
+    >
+      {symbol}
+    </div>
+  )
+}
+/** 地图操作按钮 */
+const Controls = ({ viewState, setViewState }: Props) => {
+  /** 放大，实际上是控制 viewState, 缩小同理 */
+  const zoomOut = () => {
+    setViewState({
+      ...viewState,
+      // 地图存在最大缩放级别，不能超过最大缩放级别，通常为 21
+      zoom: Math.min(viewState.zoom + 1, viewState.maxZoom),
+    })
+  }
+  const zoomIn = () => {
+    setViewState({
+      ...viewState,
+      // 地图存在最小缩放级别，不能小于最小缩放级别，通常为 1
+      zoom: Math.max(viewState.zoom - 1, viewState.minZoom),
+    })
+  }
+  return (
+    <div className="absolute bottom-20px right-20px ease-in-out duration-300 right z-2 w-30px">
+      <div className="bg-white rounded-md shadow-md relative">
+        <Button symbol="+" onClick={zoomOut} />
+        <Button symbol="-" onClick={zoomIn} />
+      </div>
+    </div>
+  )
+}
+
+export default Controls
--- src/views/MapPage/index.tsx
+++ src/views/MapPage/index.tsx
@@ -1,25 +1,37 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import Map from 'react-map-gl'
 import mapboxgl from 'mapbox-gl'
 import DeckGL from '@deck.gl/react/typed'
+import { debounce } from 'lodash'
+import Controls from './Controls'
 
 type Props = {}
 
 // @ts-ignore
 mapboxgl.accessToken = MapboxAccessToken
 
+const INITIAL_VIEW_STATE = {
+  longitude: 116.46,
+  latitude: 39.92,
+  zoom: 4,
+}
 const MapPage = ({}: Props) => {
+  const [viewState, setViewState] = useState(INITIAL_VIEW_STATE)
+  // 监听地图视图的变化，变化后从新 setViewState, 从而触发地图的重新渲染
+  const onViewStateChange = debounce((viewStateProps) => {
+    const { viewState } = viewStateProps
+    setViewState(viewState)
+  }, 200)
+  const setViewStateFunc = useCallback(setViewState, [setViewState])
   return (
     <DeckGL
       controller
-      initialViewState={{
-        longitude: 116.46,
-        latitude: 39.92,
-        zoom: 4,
-      }}
+      initialViewState={{ ...viewState }}
       style={{ width: '100%', height: '100%' }}
+      onViewStateChange={onViewStateChange}
     >
       <Map attributionControl mapStyle="mapbox://styles/mapbox/light-v10" />
+      <Controls viewState={viewState} setViewState={setViewStateFunc} />
     </DeckGL>
   )
 }
